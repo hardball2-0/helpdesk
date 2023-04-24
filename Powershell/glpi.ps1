5972dabc2e8f6425ce8d8f7c3524e105f7369eb2
hardball2-0 <93127333+hardball2-0@users.noreply.github.com>
2023-04-24 14:28:46 +0200

    Add files via upload

---
+++ Powershell/glpi.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+# Define the path to the MSI file
+$msiPath = "C:\Scripts\GLPI-Agent-1.4-x64.msi"
+
+# Define the arguments for the MSI installation
+$msiArgs = "/quiet /i ""$msiPath"" RUNNOW=1 SERVER=""http://192.168.1.68/glpi/front/inventory.php"""
+
+# Start the MSI installation process
+Start-Process -FilePath "msiexec.exe" -ArgumentList $msiArgs -Wait
+
+
+
