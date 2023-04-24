67197e83a98a8a2844f1f0645be5983eaafc9837
hardball2-0 <93127333+hardball2-0@users.noreply.github.com>
2023-04-24 14:29:30 +0200

    Update glpi.ps1

--- Powershell/glpi.ps1
+++ Powershell/glpi.ps1
@@ -4,7 +4,7 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 $msiPath = "C:\Scripts\GLPI-Agent-1.4-x64.msi"
 
 # Define the arguments for the MSI installation
-$msiArgs = "/quiet /i ""$msiPath"" RUNNOW=1 SERVER=""http://192.168.1.68/glpi/front/inventory.php"""
+$msiArgs = "/quiet /i ""$msiPath"" RUNNOW=1 SERVER=""http://IPServer/glpi/front/inventory.php"""
 
 # Start the MSI installation process
 Start-Process -FilePath "msiexec.exe" -ArgumentList $msiArgs -Wait
